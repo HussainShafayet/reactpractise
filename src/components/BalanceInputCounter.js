3162af8d17c05e45c9784c70cb17c74071e90790
HussainShafayet <shafayetsec09@gmail.com>
2024-08-27 23:25:59 +0600

    usestate and useeffect practise

--- src/components/BalanceInputCounter.js
+++ src/components/BalanceInputCounter.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import KeyCounter from './HOC/keyCounter';
 import {UserConsumer} from '../contexts/userContext';
 
 const BalanceInputCounter = (props) => {
     const {count, handleInput} = props;
+    const [lists, setList] = useState([]);
+    useEffect(() => {
+      setList([...lists, {name: 'Rahim'}]);
+    }, [])
+    console.log('list', lists);
+    
   return (
     <>
         <input type='number' onInput={handleInput} />
@@ -15,6 +21,9 @@ const BalanceInputCounter = (props) => {
             </div>)
           }}
         </UserConsumer>
+        {lists.map((item, index)=>
+          <span key={index}>Name: {item.name}</span>
+        )}
     </>
   )
 }
