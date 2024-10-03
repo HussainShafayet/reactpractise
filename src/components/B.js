d5c833afb74cb92018e5c9038c4862a8fc857a34
shafayet <shafayetsec09@gmail.com>
2024-10-03 14:31:42 +0600

    useReducer practise

--- src/components/B.js
+++ src/components/B.js
@@ -1,28 +1,26 @@
 import React, {useContext} from 'react'
 import Acontext from './A';
-
-const B = () => {
-    const {count} = useContext(Acontext);
-    console.log('const', count);
+import {useReducer} from 'react';
+const initialState = {
+    value: 0
+}
+function reducer(state, action) {
+    switch (action.type) {
+        case 'increase':
+            return {...state, value: state.value + action.value}
     
+        default:
+            return state;
+    }
+    
+}
+const B = () => {
+    //const {count} = useContext(Acontext);
+    //console.log('const', count);
+    const [count, dispatch] = useReducer(reducer, initialState)
   return (
     <div>
-        {/*<ThemeConsumer >
-        {({color})=>{
-            return (<div>
-              Welcome {color} 
-            </div>)
-          }}
-          {({color})=>{
-            return (
-                <>
-                   color {color}
-                </>
-            )
-          }}
-        </ThemeConsumer>*/}
-
-
+        <button onClick={()=>dispatch({type:'increase',value:1})}>Click {count.value}</button>
     </div>
   )
 }
