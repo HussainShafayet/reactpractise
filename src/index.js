9e7a5cfa7dd4d9995220fee0aa1be258e4cd8145
shafayet <shafayetsec09@gmail.com>
2025-06-20 10:44:13 +0600

    react version upgrade

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import React from 'react';
-import reactDom from 'react-dom';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
+
 import App from './App';
-
-ReactDOM.render(
-  <App />, 
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
